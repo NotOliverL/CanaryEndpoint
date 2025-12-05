1b7d32e986e40cf00c95cd84488024122cc888c8
oll <oliverholme@me.com>
2025-12-05 09:26:49 +0100

    Env

--- CanaryEndpoint/settings.py
+++ CanaryEndpoint/settings.py
@@ -8,7 +8,7 @@ from django.core.management.utils import get_random_secret_key
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
+SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
